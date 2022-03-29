02eb2c6a0c2bc20905c63a1cdd0921ba77ed9ac0
uzzzzy <a.fauzy4b@gmail.com>
2022-03-29 23:05:27 +0800

    show dialog

--- lib/components/item_list.dart
+++ lib/components/item_list.dart
@@ -22,10 +22,73 @@ class ItemList extends StatelessWidget {
               color: Colors.white,
             ),
             child: ListTile(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext builder) {
+                    return InputModal(id: index);
+                  },
+                );
+              },
               title: Text('Item ${index + 1}'),
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class InputModal extends StatelessWidget {
+  final int id;
+
+  const InputModal({
+    Key? key,
+    required this.id,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Stack(
+        clipBehavior: Clip.none,
+        children: <Widget>[
+          Positioned(
+            right: -40.0,
+            top: -40.0,
+            child: InkResponse(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: const CircleAvatar(
+                child: Icon(Icons.close),
+                backgroundColor: Colors.red,
+              ),
+            ),
+          ),
+          Form(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    child: Text("Submit $id"),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
